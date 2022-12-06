7abaae2097416647a53b598af7dd73b87a317d57
mateusdegobi <kim3dis@gmail.com>
2022-12-06 20:59:13 -0300

    style: resize padding and width

--- src/pages/Home/home.styled.ts
+++ src/pages/Home/home.styled.ts
@@ -7,25 +7,24 @@ const WIDTH_SCREEN = Dimensions.get('screen').width - PADDING_ROOT * 2;
 export const Container = styled.SafeAreaView`
   flex: 1;
   background-color: #f1f1f1;
-  padding: ${PADDING_ROOT}px;
+  margin: 15px;
 `;
 
 export const Content = styled.View`
-  margin-top: 50px;
+  margin-top: 30px;
 `;
 export const InfoArea = styled.View``;
 export const UserArea = styled.View``;
 export const UserPhoto = styled.Image``;
 
 export const AreaInfoSection = styled.View`
-  margin-top: 40px;
+  margin-top: 30px;
 `;
 export const BoxInfoSection = styled.View`
   background-color: #2f1155;
   height: 160px;
-  width: ${WIDTH_SCREEN}px;
   padding: 25px;
-  border-radius: 50px;
+  border-radius: 30px;
   justify-content: center;
 `;
 export const SideInfoBalance = styled.View``;
@@ -45,7 +44,6 @@ export const AreaActionButtons = styled.View`
   justify-content: center;
   flex-direction: row;
   height: 100px;
-  width: ${WIDTH_SCREEN}px;
 `;
 
 export const BoxButton = styled.TouchableOpacity`
